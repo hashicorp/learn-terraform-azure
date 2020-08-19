ae42a45946f8df7c01bb2bdb131e38e0cf4cc0df
Rachel Sharp <rachel@hashicorp.com>
2020-08-19 16:38:41 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "location" {}
+
+variable "admin_username" {
+  type        = "string"
+  description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = "string"
+  description = "Password must meet Azure complexity requirements"
+}
+
+variable "prefix" {
+  type    = "string"
+  default = "my"
+}
+
+variable "tags" {
+  type = "map"
+
+  default = {
+    Environment = "Terraform GS"
+    Dept        = "Engineering"
+  }
+}
+
+variable "sku" {
+  default = {
+    westus2 = "16.04-LTS"
+    eastus  = "18.04-LTS"
+  }
+}
