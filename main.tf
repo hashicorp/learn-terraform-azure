a5c08e06b884bada73b035702704d854299b68a4
Rachel Sharp <rachel@hashicorp.com>
2020-09-04 14:28:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}TFSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefixes       = ["10.0.1.0/24"]
 }
 
 # Create public IP
